7e1c17c46c285a6f5e34462b6d7897a1da94b0d8
Hern Yi Lee <hyileenet@gmail.com>
2025-05-13 15:21:45 +0800

    made some changes to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,21 @@ export default function App() {
   }
 
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row-reverse m-5">
+    <div className="flex flex-col items-center">
+      <div className="border-2 border-black m-2 w-[90%] max-w-[500px] bg-pink">
+        <div className="flex flex-row-reverse m-5">
         <button onClick={handleClickMusic} className="border-black border-2 rounded-2xl p-2 bg-white">Music</button>
       </div>
       <div className="flex items-center justify-center">
-        <img src="public\Logo.png" className="max-w-[500px]"/>
+        <img src="/Logo.png" className="max-w-[500px]"/>
       </div>
       
       <div className="flex sm:flex-col lg:flex-row justify-center items-center">
         <button className="border-black border-2 rounded-3xl p-4 bg-white w-[300px] m-3">Start Game</button>
         <button className="border-black border-2 rounded-3xl p-4 bg-white w-[300px] m-3">Instructions</button>
       </div>
+      </div>
+      
       
     </div>
   )
